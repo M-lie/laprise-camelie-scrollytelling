01b649616f9e19960f2ac403c8ae287c6384557f
M-lie <camelielaprise@hotmail.com>
2023-04-21 13:13:16 -0400

    p.2.2.a_b2

--- script/script.js
+++ script/script.js
@@ -1,23 +1,18 @@
 let body = document.querySelector('body');
 let timerScroll;
 
-const animation = gsap.to('.bi-chevron-compact-down', { 
-    y: '50%',
-    duration: 0.75,
-    repeat: -1,
-    yoyo: true, 
-    ease: 'power1.inOut'
-  });
-
-body.classList.remove("is-scrolling");
-
-window.addEventListener('scroll', function(timerScroll) {
-  cleartimeout(timerScroll);
+window.addEventListener('scroll', function() {
   body.classList.add("is-scrolling");
 
   timerScroll = setTimeout(function() {
     body.classList.remove("is-scrolling");
-  }, 1000);
+  }, 100);
 });
 
-
+const animation = gsap.to('.bi-chevron-compact-down', { 
+  y: '50%',
+  duration: 0.75,
+  repeat: -1,
+  yoyo: true, 
+  ease: 'power1.inOut'
+});
