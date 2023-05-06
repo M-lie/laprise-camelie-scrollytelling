57eecfc773365a7892b4eaa63b813708e84dfbc4
M-lie <camelielaprise@hotmail.com>
2023-05-06 00:40:33 -0400

    p.2.3.a_fluidite_chapitre_correction

--- script/script.js
+++ script/script.js
@@ -21,36 +21,6 @@ const animation = gsap.to('.bi-chevron-compact-down', {
   ease: 'power1.inOut'
 });
 
-/* ---- Animation qui suit une trajectoire ---- */
-
-gsap.to(".visual.chapter2.no2", {
-  motionPath: {
-    align: ".vent",
-    path: ".vent",
-    start: 0,
-    end: 1
-  },
-  scrollTrigger: {
-    trigger: "#chapter2",
-    start: 'top 25%',
-    end: 'bottom 75%',
-    scrub: 0.5
-  }
-})
-
-/* ---- Animation qui se dessine ---- */
-
-gsap.from(".vent", {
-  scrollTrigger: {
-    trigger: "#chapter2",
-    start: 'top 25%',
-    end: 'bottom 75%',
-    scrub: 0.5
-  },
-  ease: "none",
-  drawSVG: "0% 0%"
-});
-
 /* ---- Effet parallaxe ---- */
 
 gsap.to("#parallax-bg-1", {
@@ -71,59 +41,50 @@ gsap.to("#parallax-bg-2", {
   ease: "none"
 });
 
-/* ---- Animation "restart complete reverse reset" ---- */
-
-gsap.to(".character.chapter1.no1", {
-  scrollTrigger: {
-   // markers: true,
-    start: "top 75%",
-    end: "bottom 25%",
-    toggleActions: "restart complete reverse reset",
-    trigger: "#chapter1",
-  },
-  y: "125%",
-  duration: 2,
-  ease: "power2.inOut",
-});
-
-/* ---- Animation qui joue en boucle ---- */
-
-gsap.to(".character.chapter6.no1", {
-  scrollTrigger: {
-   // markers: true,
-    start: "top 75%",
-    end: "bottom 25%",
-    trigger: "#chapter6",
-  },
-  y: "-500%",
-  duration: 5,
-  repeat: -1,
-  ease: "sine.in",
-});
-
 /* ---- Animation chapitre 1 ---- */
 
 gsap.timeline({
     scrollTrigger: {
+      pin: true,
+      scrub: true,
+      toggleActions: "restart complete reverse reset",
       trigger: "#chapter1"
-    }
+    },
   })
   
-  .from("#chapter1 h2", {opacity: 0, duration: 4})
+  .from(".character.chapter1.no1", {y: "-400%", duration: 2, ease: "power2.inOut"})
+  .to("#chapter1 h2", {opacity: 0, duration: 2}, "-=1")
+  .from(".p-chapter1", {y: "300%", duration: 3, ease: "power2.inOut"}, "<")
+  .from(".character.chapter1.no2",{y: "300%", duration: 4, ease: "power2.inOut"}, "-=4");
 
-  /* ---- Animation chapitre 2 ---- */
+/* ---- Animation chapitre 2 ---- */
 
 gsap.timeline({
   scrollTrigger: {
+    pin: true,
+    markers: true,
+    end: "bottom 0",
+    toggleActions: "restart complete reverse reset",
     trigger: "#chapter2",
-    markers: true
   }
 })
     
-  .from("#chapter2 h2", {opacity: 0, duration: 4})
-  .to(".visual.chapter2.no3", {scale: 5, duration: 2});
+  .to("#chapter2 h2", {opacity: 0, duration: 2})
+  .from(".p-chapter2-1.no1", {x: "-100%", duration: 3, ease: "power2.inOut"}, "+=0.25")
+  .from(".p-chapter2-1.no2", {x: "100%", duration: 3, ease: "power2.inOut"}, "<")
+  .from(".ixy-foret", {x: "-100%", duration: 8}, "-=2")
+  .from(".vent", {ease: "none", drawSVG: "0% 0%"}, "+=1")
+  .to(".p-chapter2-1.no1", {opacity: 0}, "<")
+  .to(".p-chapter2-1.no2", {opacity: 0}, "<")
+  .to(".visual.chapter2.no2", {motionPath: {align: ".vent", path: ".vent", start: 0, end: 1}}, "<")
 
-   /* ---- Animation chapitre 3 ---- */
+  .to(".ixy-foret", {opacity: 0}, "+=0.1")
+  .to(".vent", {opacity: 0}, "<")
+  .from(".p-chapter2-2", {y: "500%", duration: 3, ease: "power2.inOut"}, "+=0.25")
+  .to(".visual.chapter2.no2", {opacity: 0}, "<")
+  .to(".bgt.chapter2.no1", {opacity: 1}, "<")
+  
+  /* ---- Animation chapitre 3 ---- */
 
 gsap.timeline({
   scrollTrigger: {
@@ -135,35 +96,61 @@ gsap.timeline({
   .from(".visual.chapter3.no1", {x: "100%", duration: 3, ease: "expo.Out"})
   .to(".visual.chapter3.no4", {scale: 2, duration: 2});
 
-  /* ---- Animation chapitre 4 ---- */
+/* ---- Animation chapitre 4 ---- */
 
 gsap.timeline({
   scrollTrigger: {
+    pin: true,
+    scrub: true,
+    toggleActions: "restart complete reverse reset",
     trigger: "#chapter4"
   }
 })
 
-  .from("#chapter4 h2", {opacity: 0, duration: 4});
+  .to("#chapter4 h2", {opacity: 0, duration: 2})
+  .from(".p-chapter4", {x: "-60%", duration: 3, ease: "power2.inOut"}, "-=0.5");
 
   /* ---- Animation chapitre 5 ---- */
 
 gsap.timeline({
   scrollTrigger: {
+    pin: true,
+    scrub: true,
+    toggleActions: "restart complete reverse reset",
     trigger: "#chapter5"
   }
 })
 
-  .from("#chapter5 h2", {opacity: 0, duration: 4})
-  .from(".visual.chapter5.no1", {x: "100%", duration: 2, ease: "power3.inOut"});
+  .to("#chapter5 h2", {opacity: 0, duration: 2})
+  .from(".p-chapter5-1", {y: "525%", duration: 1, ease: "power2.inOut"}, "-=0.5")
+  .from(".visual.chapter5.no1", {x: "100%", duration: 2, ease: "power2.inOut"}, "+=1")
+
+  .to(".character.chapter5.no1", {x: "-100%", duration: 2, ease: "power2.inOut"}, "<")
+  .to(".p-chapter5-1", {x: "-300%", duration: 2, ease: "power2.inOut"}, "<")
+  .from(".p-chapter5-2", {x: "-100%", duration: 2, ease: "power2.inOut"}, "<")
+
+  .from(".bgt.chapter5.no1", {x: "-100%", duration: 2, ease: "power2.inOut"}, "+=1")
+  .to(".visual.chapter5.no1", {x: "200%", duration: 2, ease: "power2.inOut"}, "<")
+  .to(".p-chapter5-2", {x: "200%", duration: 2, ease: "power2.inOut"}, "<")
+  .from(".p-chapter5-3", {x: "-400%", duration: 2, ease: "power2.inOut"}, "<");
+
 
   /* ---- Animation chapitre 6 ---- */
 
 gsap.timeline({
   scrollTrigger: {
+    pin: true,
+    toggleActions: "restart complete reverse reset",
     trigger: "#chapter6"
   }
 })
 
-  .from("#chapter6 h2", {opacity: 0, duration: 4})
-  .from(".character.chapter6.no1", {y: "500%", duration: 5, repeat: -1, ease: "sine.in"});
+  .to("#chapter6 h2", {opacity: 0, duration: 2})
+  .from(".p-chapter6-1", {x: "400%", duration: 2, ease: "power2.inOut"})
 
+  .from(".bg.chapter6.no1", {y: "100%", duration: 2, ease: "power2.inOut"}, "+=3")
+  .to(".p-chapter6-1", {opacity: 0}, "<")
+  .to(".ixy-ciel", {opacity: 0}, "<")
+  .from(".p-chapter6-2", {x: "100%", duration: 2, ease: "power2.inOut"}, "<")
+  .from(".character.chapter6.no1", {y: "500%", duration: 5, repeat: -1, ease: "sine.in"}, "<");
+  
